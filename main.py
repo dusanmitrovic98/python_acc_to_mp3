02800289ec525fd3eb2d06292e2ee88c0cc44314
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-11-26 17:12:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,4 @@ def convert_acc_to_mp3(input_folder, output_folder):
         os.makedirs(output_folder)
 
     # Iterate through all files in the input folder
+    for filename in os.listdir(input_folder):
