02bb1908dd2a8ce96d2baa320e18a91d790764d9
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-11-26 17:09:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ import sys
 def convert_acc_to_mp3(input_folder, output_folder):
     """
     This function converts ACC files to MP3 files in the specified input_folder
+    and saves them in the output_folder. Input folder needs to contain only ".acc" files.
